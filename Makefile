0c5fbcf2e6a528477636825b5a5d1a1d035aafb0
exitstop@list.ru <exitstop@list.ru>
2017-06-20 04:46:30 +0300

    libevent

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: server
 
 server: server.c
-		gcc -ggdb -O0 -o server server.c -ldmalloc
+		gcc -ggdb -O0 -o server server.c -levent
 clean: 
 		rm server
--- server.c
+++ server.c
@@ -1,28 +1,54 @@
-#include <stdio.h>
-#include <sys/types.h>          
+#include <event2/event.h>
+#include <event2/bufferevent.h>
 #include <sys/socket.h>
-#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]){
-    int MasterSocket = socket(
-                AF_INET,
-                SOCK_STREAM,
-                IPPROTO_TCP);
-    struct sockaddr_in SockAddr;
-    SockAddr.sin_family = AF_INET;    
-    SockAddr.sin_port = htons(12345); 
-    SockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    bind(MasterSocket, (struct sockaddr *)(&SockAddr), sizeof(SockAddr));
+void bufev_callback( struct bufferevent *buf_ev, short events, void *ptr )
+{
+  if( events & BEV_EVENT_CONNECTED )
+  {
+    printf( "Соединение установлено\n" );
+    /* Здесь можно было бы выполнить и более полезные операции,
+     * например, чтение или запись данных
+     */
+  }
+  else if( events & BEV_EVENT_ERROR )
+  {
+    exit(-1);
+    /* Перед принудительным завершением программы при необходимости
+     * выполняются "спасательные действия" (освобождение памяти,
+     * закрытие файлов и т.д.), если это возможно
+     */
+  }
+}
 
-    listen(MasterSocket, SOMAXCONN);
-    while(1){
-        int SlaveSocket = accept(MasterSocket, 0,0);
-        
-        int Buffer[5] = {0, 0, 0, 0, 0};
-        recv(SlaveSocket, Buffer, 4, MSG_NOSIGNAL);
-        shutdown(SlaveSocket, SHUT_RDWR);
-        close(SlaveSocket);
-        printf("%s\n", Buffer);
-    }
-    return 0;
- }
+int main( int argc, char* argv[] )
+{
+  struct event_base *base;
+  struct bufferevent *buf_ev;
+  struct sockaddr_in sin;
+
+  base = event_base_new();
+
+  memset( &sin, 0, sizeof(sin) );
+  sin.sin_addr.s_addr = inet_addr( "127.0.0.1" );
+  sin.sin_port = htons( 8080 );
+
+  buf_ev = bufferevent_socket_new( base, -1, BEV_OPT_CLOSE_ON_FREE );
+  bufferevent_setcb( buf_ev, NULL, NULL, bufev_callback, NULL );
+
+  if( bufferevent_socket_connect( buf_ev, (struct sockaddr *)&sin, sizeof(sin) ) < 0 )
+  {
+    /* Попытка установить соединение была неудачной */
+    bufferevent_free( buf_ev ); /* сокет закроется автоматически; см. флаг при создании */
+    return -1;
+  }
+
+  event_base_dispatch( base );
+  return 0;
+}
+
+
